d9c1c1e905adc59f6cba0877da4dc6ff807a4873
Dikshant Dwivedi <dikshantdwivedi055@gmail.com>
2021-11-21 12:58:22 +0530

    setup react router

--- src/App.js
+++ src/App.js
@@ -1,13 +1,16 @@
-import { FaqsContainer } from "./containers/faqs"
-import { FooterContainer } from "./containers/footer"
-import { JumbotronContainer } from "./containers/jumbotron"
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import * as ROUTES from "./constants/routes"
+import { Home, Browse, Signin, Signup } from "./pages"
 
 export default function App() {
   return (
-    <>
-      <JumbotronContainer />
-      <FaqsContainer />
-      <FooterContainer />
-    </>
+    <Router>
+      <Routes>
+        <Route exact path={ROUTES.HOME} element={<Home />} />
+        <Route exact path='/signin' element={<Signin />} />
+        <Route exact path='/signup' element={<Signup />} />
+        <Route exact path='/browse' element={<Browse />} />
+      </Routes>
+    </Router>
   )
 }
