0e005b643adbb83e5079a18be0cc64cac8b4103f
Dikshant Dwivedi <dikshantdwivedi055@gmail.com>
2021-11-24 10:39:26 +0530

    made configuration changes

--- src/lib/firebase.prod.js
+++ src/lib/firebase.prod.js
@@ -3,16 +3,8 @@ import "firebase/compat/firestore"
 import "firebase/compat/auth"
 //import { seedDatabase } from "../seed"
 
-const config = {
-  apiKey: "AIzaSyAiOWb4r1zuxb50ThrxGiAT0f7L84GxP9A",
-  authDomain: "netflix-clone-6dc4d.firebaseapp.com",
-  projectId: "netflix-clone-6dc4d",
-  storageBucket: "netflix-clone-6dc4d.appspot.com",
-  messagingSenderId: "60381374406",
-  appId: "1:60381374406:web:81e2c3f15cbc5c678534c7",
-}
-
-const firebase = Firebase.initializeApp(config)
+const firebaseConfig = JSON.parse(process.env.REACT_APP_FIREBASE_CREDS)
+const firebase = Firebase.initializeApp(firebaseConfig)
 
 //seedDatabase(firebase)
 
